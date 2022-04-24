4bd33bd0a7e727b4a6efd579f2cbc153f6c0a626
flav1o <flaviodanielmartinscosta@gmail.com>
2022-04-24 16:38:31 +0100

    build(mongodb): add conection to mongodb and add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/pcdigascraper', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      typePaths: ['./**/*.graphql'],
+      driver: ApolloDriver,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql-schema.ts'),
+        customScalarTypeMapping: {
+          DateTime: 'Date',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
